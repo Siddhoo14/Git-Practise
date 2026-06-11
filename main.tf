79daf7d1a2e296ac77ffd898b45fb7d80417144f
Siddhoo14 <Siddharthkanojiya1408@gmail.com>
2026-06-11 21:23:30 +0530

    resource block

--- main.tf
+++ main.tf
@@ -8,4 +8,10 @@ terraform {
 provider "azurerm" {
   features {}
 }
-resource "azurerm_storage_account""example"{}
+resource "azurerm_storage_account""example"{
+    name                     = "examplestorageaccount"
+    resource_group_name      = "example-resources"
+    location                 = "eastus"
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
